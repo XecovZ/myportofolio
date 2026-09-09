842b65d5f7a85640536110752b24349bb5762b67
Fatih <muhammadfatihd@gmail.com>
2026-09-09 15:17:48 +0700

    feat: Add achievement model

--- main/models.py
+++ main/models.py
@@ -27,3 +27,17 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+    
+    
+# Create your models here.
+class Achievement(models.Model):
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    organizer = models.CharField(max_length=255)
+    description = models.TextField()
+    achieved_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
